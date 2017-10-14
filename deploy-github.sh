438196b5c2d797531eb617193eab05d79bd9da20
Emanuel G <emanuelgdesouza@gmail.com>
2017-10-14 00:02:33 -0300

    [ new ] - deploy script

---
+++ deploy-github.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# clear dist folder
+yarn run clean
+
+# build the project
+yarn run build
+
+# deploy project
+cd dist
+rm -rf .git/
+git init
+git remote add origin "https://github.com/emanuelgsouza/debate3d-landing-page.git"
+git add .
+git commit -am "deploy"
+git push origin master:gh-pages --force
