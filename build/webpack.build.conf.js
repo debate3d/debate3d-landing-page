be63de25e8e734730ffe839758ff2f6b79467a7c
EmanuelGSouza <emanuelgdesouza@gmail.com>
2018-01-22 23:05:39 -0200

    - added service worker support

--- build/webpack.build.conf.js
+++ build/webpack.build.conf.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const fs = require('fs')
 const { defaultsDeep } = require('lodash')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
@@ -6,6 +7,15 @@ const baseWepackConfig = require('./webpack.base.conf')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
+const UglifyJS = require('uglify-es')
+
+const loadMinified = filePath => {
+  const code = fs.readFileSync(filePath, 'utf-8')
+  const result = UglifyJS.minify(code)
+  if (result.error) return ''
+  return result.code
+}
 
 const resolve = dir => path.join(__dirname, '..', dir)
 
@@ -23,7 +33,8 @@ const config = defaultsDeep(baseWepackConfig, {
         removeComments: true,
         collapseWhitespace: true,
         removeAttributeQuotes: true
-      }
+      },
+      serviceWorkerLoader: `<script>${loadMinified(resolve('build/service-worker-prod.js'))}</script>`
     }),
     new ExtractTextPlugin({
       filename: 'css/[contentHash].css'
@@ -33,7 +44,14 @@ const config = defaultsDeep(baseWepackConfig, {
         safe: true
       }
     }),
-    new UglifyJsPlugin()
+    new UglifyJsPlugin(),
+    new SWPrecacheWebpackPlugin({
+      cacheId: 'debate3d-landing-page',
+      filename: 'service-worker.js',
+      staticFileGlobs: ['dist/**/*.{js,html,css}'],
+      minify: true,
+      stripPrefix: 'dist/'
+    })
   ]
 })
 
