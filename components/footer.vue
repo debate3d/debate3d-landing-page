2adf2cf44a548604b03bf29cd1a9d633e214edc2
Emanuel G <emanuelgdesouza@gmail.com>
2018-01-23 23:39:53 -0200

    - added nuxt template

---
+++ components/footer.vue
@@ -0,0 +1,35 @@
+<script>
+  export default {
+    name: 'app-footer'
+  }
+</script>
+
+<template>
+  <footer class="landing-footer has-text-centered content">
+
+    <div class="landing-footer-links">
+      <p class="subtitle"> Links importantes </p>
+      <a href="mailto:debated3d@gmail.com" title="Envie-nos um email">debated3d@gmail.com</a><br>
+      <a href="#/app/terms-of-use">Termos de uso da plataforma</a>
+    </div>
+
+    <div class="landing-footer-social">
+      <a class="social-icons" href="https://www.facebook.com/debate3d"><i class="fa fa-facebook-official fa-2x icon" aria-hidden="true"></i></a>
+      <a class="social-icons" href="https://twitter.com/Debate_3D"><i class="fa fa-twitter fa-2x icon" aria-hidden="true"></i></a>
+      <a class="social-icons" href="https://www.instagram.com/debate_3d/"><i class="fa fa-instagram fa-2x icon" aria-hidden="true"></i></a>
+      <a class="social-icons" href="https://www.youtube.com/channel/UCmwc3dhqE9c4o3aaF6t2TBw"><i class="fa fa-youtube-play fa-2x icon" aria-hidden="true"></i></a>
+      <a class="social-icons" href="https://www.linkedin.com/company/16230450/"><i class="fa fa-linkedin-square fa-2x icon" aria-hidden="true"></i></a>
+      <a class="social-icons" href="https://plus.google.com/104194392372967768540"><i class="fa fa-google-plus fa-2x icon" aria-hidden="true"></i></a>
+    </div>
+
+    <p>
+      Desenvolvido por
+      <a href="http://emanuelgdev.com.br" title="Ir para o site de Emanuel G Web Developer">
+        EmanuelGDev
+      </a>
+      e mantido por <a href="https://www.facebook.com/AlanCostaSM" title="Conheça o perfil de Alan Costa, mantenedor da plataforma Debate3D">
+        Alan Costa
+      </a>
+    </p>
+  </footer>
+</template>
---
+++ components/header.vue
@@ -0,0 +1,60 @@
+<script>
+  export default {
+    name: 'app-header',
+    data: () => ({
+
+    })
+  }
+</script>
+
+<template>
+  <section class="hero is-info is-fullheight parallax">
+    <div class="hero-head">
+      <header class="navbar">
+
+        <div class="navbar-brand">
+          <span class="navbar-item">
+            <img src="~/assets/img/icon.png" alt="Logo">
+          </span>
+          <span class="is-uppercase is-size-3 is-size-4-mobile">
+            <b>Debate<c style= color:#00AFEF>3</c><d style= color:#ED3237>d</d></b>
+          </span>
+          <span class="navbar-burger burger" data-target="navbarMenuHeroC">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </div>
+
+        <div id="navbarMenuHeroC" class="navbar-menu">
+          <div class="navbar-end">
+            <span class="navbar-item">
+              <a href="https://debate3d.com.br/app/#/auth/register">
+                Cadastre-se
+              </a>
+            </span>
+            <span class="navbar-item">
+              <a class="button is-info is-inverted" href="https://debate3d.com.br/app/#/auth/login">
+                Login
+              </a>
+            </span>
+          </div>
+        </div>
+
+      </header>
+    </div>
+
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <h1 class="title is-uppercase">
+  				<b>Uma nova experiência de debate</b>
+  			</h1>
+        <h2 class="subtitle">
+  				Crie seu melhor argumento
+  			</h2>
+
+        <a class="button is-link is-large" href="https://debate3d.com.br/app/#/app/dashboard">Explore</a>
+      </div>
+    </div>
+  </section>
+</template>
